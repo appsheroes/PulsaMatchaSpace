6dfe24856ab8df074579f64d8dd78e900773eec1
Denis Denisov <denis.denisov8701@gmail.com>
2026-06-21 15:31:18 +0200

    Initial Commit

---
+++ PulsaMatchaSpace/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PulsaMatchaSpace
+//
+//  Created by Denis Denisov on 21/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PulsaMatchaSpace/PulsaMatchaSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulsaMatchaSpaceApp.swift
+//  PulsaMatchaSpace
+//
+//  Created by Denis Denisov on 21/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct PulsaMatchaSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
